c9a228e30554a7dd74ef476d49acc4f03cb3fc4d
)- <a-@-s-MacBook-Pro.local>
2022-07-07 12:22:22 -0400

    wip: render data and dummy data seed added to render in template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.shortcuts import render
 
 
@@ -7,3 +8,22 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+class Finch:  
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'foul little demon', 3),
+  Finch('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Finch('Raven', 'black tripod', '3 legged cat', 4),
+  Finch('In Hat', 'Siamese', 'hairless', 4),
+
+]
+    
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches })
