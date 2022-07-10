da9ad1eeaf0076a882f05bed9cb987613ca6bbcc
)- <a-@-s-MacBook-Pro.local>
2022-07-10 06:36:06 -0400

    create route added

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 #from unicodedata import name
 from django.shortcuts import render
 from .models import Finch
+from django.views.generic.edit import CreateView
 
 
 # Create your views here.
@@ -35,4 +36,6 @@ def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', {'finch' : finch})
 
-
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
