04137d40ffbed4fff49961c510557a4c4aaed797
)- <a-@-s-MacBook-Pro.local>
2022-07-11 16:56:06 -0400

    feeding feature added, we can now add a feeding time for our finch

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 #from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Finch
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import FeedingForm
@@ -38,6 +38,19 @@ def finches_detail(request, finch_id):
   return render(request, 'finches/detail.html', {
     'finch' : finch , 'feeding_form' : feeding_form
     })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+
+
+
+
 
 
 class FinchCreate(CreateView):
